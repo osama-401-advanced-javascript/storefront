b7aa3eb6d5abeae9a040c970671461c60d2be72b
Othabteh <o.thabteh@gmail.com>
2020-11-29 16:24:41 +0200

    lab36

---
+++ storefront/src/store/categories.js
@@ -0,0 +1,38 @@
+const initialState = {
+  categories: [
+    {
+      name: 'electronics',
+      displayName: 'Elecronics',
+    },
+    {
+      name: 'food',
+      displayName: 'Food',
+    },
+    {
+      name: 'clothing',
+      displayName: 'Clothing',
+    },
+  ],
+  activeCategory: 'electronics',
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+
+  // Not changing properly
+  switch (type) {
+    case 'change':
+      let activeCategory = payload.name;
+      console.log('ACTIVE CATEGORY: ', activeCategory);
+      return { ...state, activeCategory };
+    default:
+      return state;
+  }
+};
+
+export const activeCategory = (name) => {
+  return {
+    type: 'change',
+    payload: name,
+  };
+};

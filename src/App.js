c9b17e9c4cc3eeca04d75b638863562832820f99
Othabteh <o.thabteh@gmail.com>
2020-12-02 07:42:46 +0200

    lab37

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Categories from './components/Storefront/Categories';
+import Cart from './components/Storefront/Cart';
 import CurrentCategory from './components/Storefront/Current-Category';
 import Products from './components/Storefront/Products';
 // App component serves as the container for all sub-components of this application
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Header />
+      <Cart />
       <CurrentCategory />
       <Categories />
       <Products />
--- src/components/Storefront/Products.js
+++ src/components/Storefront/Products.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { add } from '../../store/actions';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-//TODO: Displays a list of products associated with the selected category
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 const Products = (props) => {
-  // console.log('PROPS???', props);
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <h2>A list of all my cool products:</h2>
       <ul>
         {props.productsToDisplay.map((product) => {
           return (
             <div key={product.name}>
-              <li key={product.name}>
+              <li onClick={() => props.addItemToCart(product)} key={product.name}>
                 <strong> {product.name} </strong>
                 <span key={product.price}> {product.price}</span>
                 <span key={product.inStock}> {product.inStock}</span>
+                <Button variant='contained' color='primary'>
+                  {product.name}
+                </Button>
               </li>
               <img src={product.image} alt={product.name} width='100' height='100'></img>
             </div>
@@ -26,6 +38,7 @@ const Products = (props) => {
   );
 };
 
+const mapDispatchToProps = { addItemToCart: add };
 const mapStateToProps = (state) => {
   // console.log('STATE???', state.products.products);
   return {
@@ -34,4 +47,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(Products);
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
